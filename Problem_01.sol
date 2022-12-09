ec5d90c214991e669e79fe3e6885d0cfdfea7070
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2022-12-09 11:52:28 +0600

    Update Problem_01.sol

--- Problem_01.sol
+++ Problem_01.sol
@@ -1,9 +1,18 @@
 
-// 1.Write a function that takes an array of integers and returns the sum of the elements.
-function sum(int[] memory _values) public view returns (int _sum) {
-    // iterate over the array and add each element to the sum
-    for (uint i = 0; i < _values.length; i++) {
-        _sum += _values[i];
+//Write a function that takes an array of integers and returns the sum of the elements.
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Problem01{
+
+    function sum(int[] memory _values) public pure returns (int _sum) {
+        // iterate over the array and add each element to the sum
+        for (uint i = 0; i < _values.length; i++) {
+            _sum += _values[i];
+        }
+        return _sum;
     }
-    return _sum;
+
 }
